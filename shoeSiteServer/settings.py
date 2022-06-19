f201df85ebf627119d51edcd84793155f7239241
ngovandong <dongngo2001@gmail.com>
2022-06-19 15:59:26 +0700

    add static folder

--- shoeSiteServer/settings.py
+++ shoeSiteServer/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1qiajw*w*z5p&%%6tx^-(u%ajct@ca&_yhrs1q3ut%%qr2kter
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ngovandong.pythonanywhere.com']
 CORS_ALLOWED_ORIGINS = ['http://localhost:8080',
                         'https://3dshoestore.netlify.app/']
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -138,3 +138,4 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
